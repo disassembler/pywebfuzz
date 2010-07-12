74412288d373404545c82eaa8e7d35696b11b7e0
nhamiel <nhamiel@82fd2642-525a-7321-434f-1f2799a5e545>
2010-07-12 17:20:06 +0000

    Changed the "head" value in the request logic to "headers" for clarity. Also made some comment changes as well.
    git-svn-id: http://pywebfuzz.googlecode.com/svn/trunk@7 82fd2642-525a-7321-434f-1f2799a5e545

--- pywebfuzz/utils.py
+++ pywebfuzz/utils.py
@@ -7,26 +7,31 @@ import urllib
 import urllib2
 from EncoderLib import *
 
-def make_request(location, verb="GET", postdata=None, head=None):
+def make_request(location, verb="GET", postdata=None, headers=None):
     """ This provides a convenience function for making requests. This interfaces
     with urllib2 and provides the ability to make GET and POST requests for content.
     The return data from this function is headers and content"""
     
     # Checks to ensure that if it is a POST request that postdata is present
+    # I may remove this because it could potentially be a valid test case w/o data
     if verb == "POST" and postdata == None:
         print("You did not specify any associated postdata and this is a POST request")
         sys.exit(1)
     
     # Checks to ensure that header values and postdata are in the appropriate format
-    if type(head) != dict and head != None:
+    if type(headers) != dict and headers != None:
         raise TypeError, ("headers are not a valid Python dictionary")
     if type(postdata) != str and postdata != None:
         raise TypeError, ("postdata is not a valid Python string")
     
+    ####
+    # Todo: Add support for more HTTP methods
+    ####
+    
     # Takes the appropriate actions based on the request type
     if verb == "GET":
-        if head:
-            req = urllib2.Request(location, headers=head)
+        if headers:
+            req = urllib2.Request(location, headers=headers)
         else:
             req = urllib2.Request(location)
             
@@ -37,8 +42,8 @@ def make_request(location, verb="GET", postdata=None, head=None):
         return(headers, content)
         
     elif verb == "POST":
-        if head:
-            req = urllib2.Request(location, headers=head)
+        if headers:
+            req = urllib2.Request(location, headers=headers)
         else:
             req = urllib2.Request(location)
             
@@ -48,9 +53,12 @@ def make_request(location, verb="GET", postdata=None, head=None):
         headers = response.info()
         content = response.read()
         return(headers, content)
+        
+    else:
+        print("Invalid verb for make_request function")
     
 def generate_range(start, stop, step=1, pre=None, post=None):
-    """ Generate a range of values with optional stepping as well as chars prepended or attached to
+    """ Generate a range of values with optional stepping. Chars can be prepended or attached to
     the end of each value that is generated """
     
     rangevals = range(start, stop, step)
